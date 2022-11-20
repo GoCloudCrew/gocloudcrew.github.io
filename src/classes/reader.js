8fbff3d8c0e8adb5bd1973b7bc7b77daa3b6a3e4
Dan Abarbanel <abarbaneld@gmail.com>
2022-11-20 12:45:28 -0500

    Switch to yaml

--- src/classes/reader.js
+++ src/classes/reader.js
@@ -1,18 +1,26 @@
+import { parse } from 'yaml';
+
 class Reader {
     static async load(page){
-        const response = await fetch(`/data/pages/${page}.json`);
-        const data = await response.json();
-        return data;
+        const response = await fetch(`/data/pages/${page}.yaml`);
+        const data = await response.text();
+
+        const result = parse(data);
+        return result.data;
     }
     static async modules() {
-        const response = await fetch(`/data/modules/modules.json`);
-        const data = await response.json();
-        return data;
+        const response = await fetch(`/data/modules/modules.yaml`);
+        const data = await response.text();
+
+        const result = parse(data);
+        return result.data;
     }    
     static async system() {
-        const response = await fetch(`/data/system.json`);
-        const data = await response.json();
-        return data;
+        const response = await fetch(`/data/system.yaml`);
+        const data = await response.text();
+
+        const result = parse(data);
+        return result.data;
     }
 }
 
